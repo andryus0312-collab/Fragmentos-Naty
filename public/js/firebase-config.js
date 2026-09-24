d41a6fb032c5bd69859db0e6d5df9324000b88a8
Sr. Andryus <andryus0312@gmail.com>
2026-09-24 15:53:32 -0500

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,23 +1,17 @@
 // 📍 public/js/firebase-config.js
-// Importar las funciones necesarias desde los servidores de Firebase (CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// PEGA AQUÍ TUS CREDENCIALES DE FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyAGSV6Oc9iGAcsxTMLw9GyILQrbNDecLJ0",
+  apiKey: "TU_API_KEY_AQUI",
   authDomain: "fragmentos-naty.firebaseapp.com",
   projectId: "fragmentos-naty",
-  storageBucket: "fragmentos-naty.firebasestorage.app",
-  messagingSenderId: "967090680217",
-  appId: "1:967090680217:web:3eeb5bc0d57c1b3414e308"
+  storageBucket: "fragmentos-naty.appspot.com",
+  messagingSenderId: "TU_ID_AQUI",
+  appId: "TU_APP_ID_AQUI"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// Exportar para usar en otros archivos
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
